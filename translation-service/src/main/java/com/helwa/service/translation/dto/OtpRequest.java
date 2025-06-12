83173c76fc72081e12a3c27966695ed150912a59
Ahmed Shaban <ahmedshabaan@outlook.com>
2025-06-12 17:14:15 +0300

    Translation service: Add OTP DTOs.

---
+++ translation-service/src/main/java/com/helwa/service/translation/dto/OtpRequest.java
@@ -0,0 +1,4 @@
+package com.helwa.service.translation.dto;
+
+public record OtpRequest(int statusCode) {
+}
--- translation-service/src/main/java/com/helwa/service/translation/dto/OtpResponse.java
+++ translation-service/src/main/java/com/helwa/service/translation/dto/OtpResponse.java
@@ -1,6 +1,6 @@
-package com.helwa.service.gateway.dto;
+package com.helwa.service.translation.dto;
 
-public record LocalizedResponseDTO(int statusCode, String message_ar, String message_en, String channel) {
+public record OtpResponse(int statusCode, String message_ar, String message_en, String channel) {
 
     // TODO: For simplicity, we return messages explicitly in the same JSON, Messages to be encapsulated in a separate JSON object and injected into the response.
 }
