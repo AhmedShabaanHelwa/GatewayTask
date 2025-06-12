bcb4bfaf44a7847c968ee9f6c233f9c36ab75843
Ahmed Shaban <ahmedshabaan@outlook.com>
2025-06-12 19:07:21 +0300

    OTP service: Enhance OtpService to call translation service.

--- otp-service/src/main/java/com/helwa/service/otp/service/OtpService.java
+++ otp-service/src/main/java/com/helwa/service/otp/service/OtpService.java
@@ -1,24 +1,49 @@
 package com.helwa.service.otp.service;
 
 import com.helwa.service.otp.dto.OTPRequest;
-import com.helwa.service.otp.dto.OTPResponse;
+import com.helwa.service.otp.dto.OtpResponse;
+import com.helwa.service.otp.dto.OtpTranslationRequest;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.Random;
 
 @Service
 public class OtpService {
 
-    @NotNull
-    public OTPResponse validateOtp(@NotNull OTPRequest otpRequest) {
-        return getRandomOTPStatus(otpRequest);
+    private final WebClient webClient;
+    private static final String TRANSLATION_SERVICE_URL = "http://localhost:5000/translate";
+
+    public OtpService() {
+        // TODO: Translation service url to be obtained dynamically using service discovery, Just hardcode it for now
+            webClient = WebClient.create(TRANSLATION_SERVICE_URL);
     }
 
     @NotNull
-    private OTPResponse getRandomOTPStatus(@NotNull OTPRequest otpRequest) {
+    public Mono<OtpResponse> validateOtp(@NotNull OTPRequest otpRequest) {
         // TODO: Implement logic to validate OTP
         // For now, just return random status
-        return new OTPResponse(new Random().nextInt(3));
+        int statusCode = getRandomOTPStatus(otpRequest);
+
+        Mono<OtpTranslationRequest> otpTranslationRequest = Mono.just(new OtpTranslationRequest(statusCode));
+        return translate(otpTranslationRequest);
+    }
+
+    private int getRandomOTPStatus(@NotNull OTPRequest otpRequest) {
+        // TODO: Implement logic to validate OTP
+        // For now, just return random status
+        return new Random().nextInt(3);
+    }
+
+    @NotNull
+    private Mono<OtpResponse> translate(@NotNull Mono<OtpTranslationRequest> otpTranslationRequest) {
+
+        return webClient.post()
+                .uri("/otp")
+                .body(otpTranslationRequest, OtpTranslationRequest.class)
+                .retrieve()
+                .bodyToMono(OtpResponse.class);
     }
 }
