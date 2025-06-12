f25b4bd6fa820d4ec2059da1dd41f0993522124a
Ahmed Shaban <ahmedshabaan@outlook.com>
2025-06-12 19:06:18 +0300

    OTP service: Add OtpResponse and OtpTranslationRequest

--- otp-service/src/main/java/com/helwa/service/otp/dto/OtpResponse.java
+++ otp-service/src/main/java/com/helwa/service/otp/dto/OtpResponse.java
@@ -1,5 +1,5 @@
 package com.helwa.service.otp.dto;
 
-public record OTPResponse(int statusCode) {
+public record OtpResponse(int statusCode, String message_ar, String message_en, String channel) {
     // TODO: Possible improvement: To make status code as enum to enhance readability
 }
---
+++ otp-service/src/main/java/com/helwa/service/otp/dto/OtpTranslationRequest.java
@@ -0,0 +1,4 @@
+package com.helwa.service.otp.dto;
+
+public record OtpTranslationRequest(int statusCode) {
+}
